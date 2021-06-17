f1d0204f41f13690be7c1b354c0dd11e5cd8255e
cjf8140 <32036089+cjf8140@users.noreply.github.com>
2021-06-17 23:59:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ function setup() {
     s_y[i] = random(height);
   }
   if (UserAgent.match(/iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i) != null || UserAgent.match(/LG|SAMSUNG|Samsung/) != null)
-
-	{
+{
     kgon=1;
   }
 }
